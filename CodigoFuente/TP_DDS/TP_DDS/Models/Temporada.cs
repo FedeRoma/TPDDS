b0b63a3866b71442a9840584b2e05cd36ab370ce
Jtonelli LN <j.tonelli90@gmail.com>
2015-09-01 20:33:05 -0300

    Avances TP

--- CodigoFuente/TP_DDS/TP_DDS/Models/Temporada.cs
+++ CodigoFuente/TP_DDS/TP_DDS/Models/Temporada.cs
@@ -7,11 +7,12 @@ namespace DDS_TP.Models
 {
     public class Temporada
     {
+        public int Id { get; set; }
         public string Nombre { get; set; }
 
-        public Temporada(string nombre)
-        {
-            Nombre = nombre;
-        }
+        //public Temporada(string nombre)
+        //{
+        //    Nombre = nombre;
+        //}
     }
 }
--- CodigoFuente/TP_DDS/TP_DDS/Models/Usuario.cs
+++ CodigoFuente/TP_DDS/TP_DDS/Models/Usuario.cs
@@ -18,88 +18,92 @@ namespace DDS_TP.Models
 	    public double Peso { get; set; }
 	    public double Altura { get; set; }
 	    public Rutina? Rutina { get; set; }
-        public CondicionPreexistente CondicionPreexistente { get; set; }
-        public ICollection<Dieta> Dieta { get; set; }
-        public ICollection<Preferencia> PreferenciasAlimenticias { get; set; }
-        public ICollection<Receta> Recetas { get; set; }
-        public ICollection<Grupo> Grupos { get; set; }
+        public int CondicionPreexistenteId { get; set; }
+        public virtual ICollection<Dieta> Dietas { get; set; }
+        public virtual ICollection<Preferencia> PreferenciasAlimenticias { get; set; }
+        public virtual ICollection<Receta> Recetas { get; set; }
+        public virtual ICollection<Grupo> Grupos { get; set; }
+        public virtual CondicionPreexistente CondicionPreexistente { get; set; }
 
-        private List<ValidacionUsuario> validaciones;
+
+
+
+        //private List<ValidacionUsuario> validaciones;
 	    
-	    public Usuario() 
-        {
-		    Initialize();
-	    }
+        //public Usuario() 
+        //{
+        //    Initialize();
+        //}
 
-        public Usuario(String nombre, String sexo, Rutina rutina, double peso, double altura, DateTime fechaNacimiento, CondicionPreexistente condicionPreexistente) 
-        {
-		    Initialize();
-		    this.Nombre = nombre;
-		    this.Sexo = sexo;
-		    this.Rutina = rutina;
-		    this.Peso = peso;
-		    this.Altura = altura;
-		    this.FechaNacimiento = fechaNacimiento;
-            this.CondicionPreexistente = condicionPreexistente;
-	    }
+        //public Usuario(String nombre, String sexo, Rutina rutina, double peso, double altura, DateTime fechaNacimiento, int condicionPreexistenteId) 
+        //{
+        //    Initialize();
+        //    this.Nombre = nombre;
+        //    this.Sexo = sexo;
+        //    this.Rutina = rutina;
+        //    this.Peso = peso;
+        //    this.Altura = altura;
+        //    this.FechaNacimiento = fechaNacimiento;
+        //    this.CondicionPreexistenteId = condicionPreexistenteId;
+        //}
 
-	    public void Initialize() 
-        {
-            //PreferenciasAlimenticias = new List<Preferencia>();
-            validaciones = new List<ValidacionUsuario>();
-            //Recetas = new List<Receta>();
-            //Grupos = new List<Grupo>();
+        //public void Initialize() 
+        //{
+        //    //PreferenciasAlimenticias = new List<Preferencia>();
+        //    validaciones = new List<ValidacionUsuario>();
+        //    //Recetas = new List<Receta>();
+        //    //Grupos = new List<Grupo>();
 
-            CargarValidadores();
-	    }
+        //    CargarValidadores();
+        //}
 
-        private void CargarValidadores()
-        {
-            validaciones.Add(new ValidacionDatos());
-            validaciones.Add(new ValidacionFechaNacimiento());
-            validaciones.Add(new ValidacionNombre());
-        }
+        //private void CargarValidadores()
+        //{
+        //    validaciones.Add(new ValidacionDatos());
+        //    validaciones.Add(new ValidacionFechaNacimiento());
+        //    validaciones.Add(new ValidacionNombre());
+        //}
 	
-	    public void Validar() 
-        {
-		    validaciones.ForEach(validacion => validacion.Validar(this));
-	    }		
+        //public void Validar() 
+        //{
+        //    validaciones.ForEach(validacion => validacion.Validar(this));
+        //}		
 	
-	    public void AgregarReceta(Receta receta)
-        {
-		    receta.ValidarReceta();
+        //public void AgregarReceta(Receta receta)
+        //{
+        //    receta.ValidarReceta();
             
-            receta.Creador = this;
-            Recetas.Add(receta);
-	    }
+        //    receta.Creador = this;
+        //    Recetas.Add(receta);
+        //}
 
-	    public void AgregarPreferencia(Preferencia preferencia)
-        {
-            PreferenciasAlimenticias.Add(preferencia);
-	    }
+        //public void AgregarPreferencia(Preferencia preferencia)
+        //{
+        //    PreferenciasAlimenticias.Add(preferencia);
+        //}
 
-        public void QuitarPreferencia(Preferencia preferencia)
-        {
-            PreferenciasAlimenticias.Remove(preferencia);
-        }
+        //public void QuitarPreferencia(Preferencia preferencia)
+        //{
+        //    PreferenciasAlimenticias.Remove(preferencia);
+        //}
 	
-	    public void AgregarGrupo(Grupo grupo)
-        {
-		    Grupos.Add(grupo);
-		    grupo.agregameALista(this);
-	    }
+        //public void AgregarGrupo(Grupo grupo)
+        //{
+        //    Grupos.Add(grupo);
+        //    grupo.agregameALista(this);
+        //}
 
-        public bool ComparteGrupoCon(Usuario creadorReceta)
-        {
-            //return Grupos.Exists(grupo => grupo.perteneceUsuarioAlGrupo(creadorReceta));
-            throw new NotImplementedException();
-        }
+        //public bool ComparteGrupoCon(Usuario creadorReceta)
+        //{
+        //    //return Grupos.Exists(grupo => grupo.perteneceUsuarioAlGrupo(creadorReceta));
+        //    throw new NotImplementedException();
+        //}
 
-        public void ActualizarPerfil(String nombre, Sexo sexo, Rutina rutina, double peso, double altura, DateTime fechaNacimiento)
-        {
-            this.Rutina = rutina;
-            this.Peso = peso;
-            this.Altura = altura;
-        }
+        //public void ActualizarPerfil(String nombre, Sexo sexo, Rutina rutina, double peso, double altura, DateTime fechaNacimiento)
+        //{
+        //    this.Rutina = rutina;
+        //    this.Peso = peso;
+        //    this.Altura = altura;
+        //}
     }
 }
